8636dd93a2937b63112336c4b907884468eb8711
Divyansh-Thapliyal <divyanshthapliyalwork007@gmail.com>
2023-07-25 15:50:36 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import {useState,useEffect} from 'react';
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+// import MoviesList from "./components/MoviesList";
+
+const apiKey='42d039fcc62cc28932ec0acf05964888';
+
 function App() {
-  return (
-    <div>
-      <h1>React app!</h1>
-    </div>
+  
+
+  return (<>
+      <Router>  
+      <Header/>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        {/* <Route path="/popular" element={<MoviesList/> } ></Route>
+        <Route path="/top-rated" element={<MoviesList/> } ></Route>
+        <Route path="/upcoming" element={<MoviesList/> }></Route> */}
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/MovieItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MovieItem.css';
+
+const MovieItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MovieItem
